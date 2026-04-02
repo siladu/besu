be07eab38a12a0671a200e9b3961cac11554b348
Simon Dudley <simon.dudley@consensys.net>
2026-04-02 22:09:01 +1000

    Add MulModOperationV2 and Benchmarks
    Signed-off-by: Simon Dudley <simon.dudley@consensys.net>
    
    # Conflicts:
    #	evm/src/main/java/org/hyperledger/besu/evm/v2/StackArithmetic.java

---
+++ evm/src/main/java/org/hyperledger/besu/evm/v2/operation/MulModOperationV2.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright contributors to Besu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.evm.v2.operation;
+
+import org.hyperledger.besu.evm.EVM;
+import org.hyperledger.besu.evm.frame.MessageFrame;
+import org.hyperledger.besu.evm.gascalculator.GasCalculator;
+import org.hyperledger.besu.evm.operation.Operation;
+import org.hyperledger.besu.evm.v2.StackArithmetic;
+
+/** The Mul mod operation. */
+public class MulModOperationV2 extends AbstractFixedCostOperationV2 {
+
+  private static final OperationResult mulModSuccess = new OperationResult(8, null);
+
+  /**
+   * Instantiates a new Mul mod operation.
+   *
+   * @param gasCalculator the gas calculator
+   */
+  public MulModOperationV2(final GasCalculator gasCalculator) {
+    super(0x09, "MULMOD", 3, 1, gasCalculator, gasCalculator.getMidTierGasCost());
+  }
+
+  @Override
+  public Operation.OperationResult executeFixedCostOperation(
+      final MessageFrame frame, final EVM evm) {
+    return staticOperation(frame, frame.stackDataV2());
+  }
+
+  /**
+   * Performs MulMod operation.
+   *
+   * @param frame the frame
+   * @return the operation result
+   */
+  public static OperationResult staticOperation(final MessageFrame frame, final long[] s) {
+    if (!frame.stackHasItems(3)) return UNDERFLOW_RESPONSE;
+    frame.setTopV2(StackArithmetic.mulMod(s, frame.stackTopV2()));
+    return mulModSuccess;
+  }
+}
