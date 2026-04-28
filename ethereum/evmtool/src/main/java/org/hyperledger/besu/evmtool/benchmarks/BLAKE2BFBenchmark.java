b2201d996009008f15769da8840ec74ac88cfae1
Simon Dudley <simon.dudley@consensys.net>
2026-04-28 17:50:01 +0200

    native support
    Signed-off-by: Simon Dudley <simon.dudley@consensys.net>

--- ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/benchmarks/BLAKE2BFBenchmark.java
+++ ethereum/evmtool/src/main/java/org/hyperledger/besu/evmtool/benchmarks/BLAKE2BFBenchmark.java
@@ -14,7 +14,7 @@
  */
 package org.hyperledger.besu.evmtool.benchmarks;
 
-import org.apache.tuweni.bytes.Bytes;
+import org.hyperledger.besu.crypto.Blake2bfMessageDigest;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.evm.EvmSpecVersion;
 import org.hyperledger.besu.evm.fluent.EvmSpec;
@@ -25,11 +25,14 @@ import java.util.LinkedHashMap;
 import java.util.Random;
 import java.util.SequencedMap;
 
+import org.apache.tuweni.bytes.Bytes;
+
 /** Benchmark BLAKE2BF precompile */
 public class BLAKE2BFBenchmark extends BenchmarkExecutor {
 
   private static final long DEFAULT_TRANSACTION_GAS_LIMIT_CAP_OSAKA = 16_777_216L;
   private static final int RANDOM_SAMPLE_SIZE = 3;
+
   /**
    * The constructor. Use default math based warmup and interations.
    *
@@ -44,22 +47,34 @@ public class BLAKE2BFBenchmark extends BenchmarkExecutor {
   public void runBenchmark(final Boolean attemptNative, final String fork) {
     EvmSpecVersion forkVersion = EvmSpecVersion.fromName(fork);
 
-    if (attemptNative != null && attemptNative) {
-      output.println("Native is unsupported, falling back to Java");
+    if (attemptNative != null
+        && (!attemptNative || !Blake2bfMessageDigest.Blake2bfDigest.maybeEnableNative())) {
+      Blake2bfMessageDigest.Blake2bfDigest.disableNative();
     }
-    output.println("Java BLAKE2BF");
+
+    output.println(
+        Blake2bfMessageDigest.Blake2bfDigest.isNative() ? "Native BLAKE2BF" : "Java BLAKE2BF");
 
     PrecompiledContract contract =
-        EvmSpec.evmSpec(forkVersion).getPrecompileContractRegistry().get(Address.BLAKE2B_F_COMPRESSION);
+        EvmSpec.evmSpec(forkVersion)
+            .getPrecompileContractRegistry()
+            .get(Address.BLAKE2B_F_COMPRESSION);
 
     final SequencedMap<String, Bytes> testCases = new LinkedHashMap<>();
-    // 213 bytes = [4 bytes for rounds][64 bytes for h][128 bytes for m][8 bytes for t_0][8 bytes for t_1][1 byte for f]
-    testCases.put("Case 1",
-            Bytes.fromHexString("0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000001"));
-    testCases.put("Case 2",
-            Bytes.fromHexString("0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000"));
-    testCases.put("Case 3",
-            Bytes.fromHexString("007a120048c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000001"));
+    // 213 bytes = [4 bytes for rounds][64 bytes for h][128 bytes for m][8 bytes for t_0][8 bytes
+    // for t_1][1 byte for f]
+    testCases.put(
+        "Case 1",
+        Bytes.fromHexString(
+            "0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000001"));
+    testCases.put(
+        "Case 2",
+        Bytes.fromHexString(
+            "0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000"));
+    testCases.put(
+        "Case 3",
+        Bytes.fromHexString(
+            "007a120048c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000001"));
     final Random random = new Random();
     int randomSampleCount = 0;
     while (randomSampleCount < RANDOM_SAMPLE_SIZE) {
@@ -72,7 +87,7 @@ public class BLAKE2BFBenchmark extends BenchmarkExecutor {
         continue;
       }
       randomSampleCount++;
-      testCases.put("Random "+randomSampleCount, Bytes.wrap(dataf));
+      testCases.put("Random " + randomSampleCount, Bytes.wrap(dataf));
     }
     precompile(testCases, contract, forkVersion);
   }
