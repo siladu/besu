29f37c305b9b0a93a530ac8e14848f8e409daf8d
Simon Dudley <simon.dudley@consensys.net>
2026-06-02 13:52:21 +1000

    Add transactionCount
    Log block json object with number, hash, gas_used, tx_count
    
    Signed-off-by: Simon Dudley <simon.dudley@consensys.net>

--- evm/src/main/java/org/hyperledger/besu/evm/tracing/SlowBlockTracer.java
+++ evm/src/main/java/org/hyperledger/besu/evm/tracing/SlowBlockTracer.java
@@ -17,9 +17,17 @@ package org.hyperledger.besu.evm.tracing;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.tuweni.bytes.Bytes;
+import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.datatypes.Log;
+import org.hyperledger.besu.datatypes.Transaction;
+import org.hyperledger.besu.evm.worldstate.WorldView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Set;
 
 public class SlowBlockTracer implements OperationTracer {
 
@@ -28,13 +36,28 @@ public class SlowBlockTracer implements OperationTracer {
 
   private long executionStartNanos;
   private long executionTimeNanos;
+  private int transactionCount;
 
   public void traceStartBlock() {
     executionStartNanos = System.nanoTime();
   }
+
   public void traceEndBlock(final long blockNumber, final Hash blockHash, final long gasUsed) {
     executionTimeNanos = System.nanoTime() - executionStartNanos;
     logSlowBlock(blockNumber, blockHash, gasUsed);
+  }
+
+  @Override
+  public void traceEndTransaction(
+      final WorldView worldView,
+      final Transaction tx,
+      final boolean status,
+      final Bytes output,
+      final List<Log> logs,
+      final long gasUsed,
+      final Set<Address> selfDestructs,
+      final long timeNs) {
+    transactionCount++;
   }
 
   private void logSlowBlock(final long blockNumber, final Hash blockHash, final long gasUsed) {
@@ -42,6 +65,12 @@ public class SlowBlockTracer implements OperationTracer {
       final ObjectNode json = JSON_MAPPER.createObjectNode();
       json.put("level", "warn");
       json.put("msg", "Slow block");
+
+      final ObjectNode blockNode = json.putObject("block");
+      blockNode.put("number", blockNumber);
+      blockNode.put("hash", blockHash.toHexString());
+      blockNode.put("gas_used", gasUsed);
+      blockNode.put("tx_count", transactionCount);
 
       final ObjectNode timingNode = json.putObject("timing");
       timingNode.put("execution_ms", executionTimeNanos / 1_000_000);
@@ -57,13 +86,13 @@ public class SlowBlockTracer implements OperationTracer {
               .addArgument(blockHash.toHexString())
               .addArgument(executionTimeNanos / 1_000_000)
               .addArgument(gasUsed)
-              .addArgument(String.format("%.2f", calculateMgasPerSecond(gasUsed)))
-              .addArgument(-1)
+              .addArgument(String.format("%.2f", calculateMGasPerSecond(gasUsed)))
+              .addArgument(transactionCount)
               .log();
     }
   }
 
-  private double calculateMgasPerSecond(final long gasUsed) {
+  private double calculateMGasPerSecond(final long gasUsed) {
     if (executionTimeNanos == 0) {
       return 0.00;
     }
